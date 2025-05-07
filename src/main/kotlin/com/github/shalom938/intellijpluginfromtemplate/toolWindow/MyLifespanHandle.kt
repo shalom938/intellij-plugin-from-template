4d1cf4ee37520aa2bb49b8694a88c1ccda9fe1f9
Shalom Ben Zvi Kazaz <shalom938@gmail.com>
2025-05-08 00:47:20 +0300

    delete unused classes

--- src/main/kotlin/com/github/shalom938/intellijpluginfromtemplate/toolWindow/MyLifespanHandle.kt
+++ src/main/kotlin/com/github/shalom938/intellijpluginfromtemplate/toolWindow/MyLifespanHandle.kt
@@ -8,34 +8,34 @@ import org.cef.handler.CefLifeSpanHandler
 
 class MyLifespanHandle: CefLifeSpanHandler {
     override fun onBeforePopup(
-        browser: CefBrowser?,
-        frame: CefFrame?,
-        target_url: String?,
-        target_frame_name: String?
+        browser: CefBrowser,
+        frame: CefFrame,
+        target_url: String,
+        target_frame_name: String
     ): Boolean {
         thisLogger().info("in onBeforePopup")
         return true
     }
 
-    override fun onAfterCreated(browser: CefBrowser?) {
+    override fun onAfterCreated(browser: CefBrowser) {
         thisLogger().info("in onAfterCreated")
         val schemeHandlerFactory = MySchemaHandlerFactory()
         CefApp.getInstance().registerSchemeHandlerFactory(
             schemeHandlerFactory.getSchema(), null, schemeHandlerFactory
         )
-        browser?.loadURL("https://main/index.html")
+        browser.loadURL("https://main/index.html")
     }
 
-    override fun onAfterParentChanged(browser: CefBrowser?) {
+    override fun onAfterParentChanged(browser: CefBrowser) {
         thisLogger().info("in onAfterParentChanged")
     }
 
-    override fun doClose(browser: CefBrowser?): Boolean {
+    override fun doClose(browser: CefBrowser): Boolean {
         thisLogger().info("in doClose")
         return true
     }
 
-    override fun onBeforeClose(browser: CefBrowser?) {
+    override fun onBeforeClose(browser: CefBrowser) {
         thisLogger().info("in onBeforeClose")
     }
 
